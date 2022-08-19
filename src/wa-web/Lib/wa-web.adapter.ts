369f74178681b1e1b52b7c95f46fba62a684475b
Enrique SOTO <57093115+staind0001@users.noreply.github.com>
2022-08-19 13:10:20 +0200

    test waweb

--- src/wa-web/Lib/wa-web.adapter.ts
+++ src/wa-web/Lib/wa-web.adapter.ts
@@ -6,15 +6,19 @@ export class WhatsappWebAdapter {
 
     async newClient(){
         const  client = new this.WhatsappWeb.Client({
-                authStrategy: new WhatsappWeb.NoAuth()
+                authStrategy: new WhatsappWeb.NoAuth(),
+                puppeteer:{headless:true}
             })
             client.initialize();
+
+            
             const QRCode = new Promise((resolve, reject) => {
                 client.on('qr', qr => { 
                 resolve(qr)})
             })
             const res =  await QRCode.then()
             return res;
+
         }
  
 }
